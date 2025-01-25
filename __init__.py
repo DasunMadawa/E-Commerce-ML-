78e13e2adf04a3fbbdc9a06fdad59a8291cdb18e
TGDRangaka <dilshanrangaka@gmail.com>
2025-01-25 12:07:22 +0530

    feat: implement inputs

--- __init__.py
+++ __init__.py
@@ -28,3 +28,27 @@ print(biscuit.get_details())
 ## Update
 biscuit.update_price(200)
 print(biscuit.get_details())
+
+#========================================================================================================================================================================================================================
+print("----------- Customer Details --------------------")
+cus_name = input("Enter Customer Name: ")
+cus_address = input("Enter Customer Address: ")
+cus_contact = input("Enter Customer Contact: ")
+
+#Customer 2
+##create
+new_customer = Customer(name=cus_name, address=cus_address, contact=cus_contact)
+print(new_customer.get_details())
+print("----------- Customer Created -----------------------------")
+print("----------------------------------------------------------")
+
+#Product 3
+print("----------- Product Details -----------------------------")
+prod_name = input("Enter Product Name: ")
+prod_quantity = int(input("Enter Product Quantity: "))
+prod_price = float(input("Enter Product Price: "))
+
+## Create
+new_product = Product(name=prod_name, price=prod_price, quantity=prod_quantity)
+print(new_product.get_details())
+print("----------- Product Created -----------------------------")
